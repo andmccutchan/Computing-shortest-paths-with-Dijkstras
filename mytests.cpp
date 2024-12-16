3191ab0ceff6357dbd33e71b0082895db881ce63
namuleekim <kim_n1@denison.edu>
2024-12-16 14:47:47 -0500

    Reapply "Fix mytests infinite loop, tidy output"
    This reverts commit ef2111082142ab91095fc7d73ac02df5334c165f.

--- mytests.cpp
+++ mytests.cpp
@@ -1,17 +1,26 @@
 #include <iostream> 
 #include "weightedGraph.hpp"
 #include "priorityQueue.hpp"
+#include "weightedGraph.cpp"
+#include "priorityQueue.cpp"
 using namespace std;
 
+
+// Test functions declared here
+// Weighted Graph test functions
+// Priority Que test functions
+// Disjkstra's test functions
+//============================================================
+
+// CLI function declared here
+//============================================================
+
 int main (void) {
+// Testing or CLI prompt here
 
-
-
-    
-
-
-    //=============================================================
-    WeightedGraph<long> graph;
+// CLI function (move out of main later)
+//=============================================================
+    WeightedGraph<long long> graph; //Windows compatible, Window long = 32bits.
     
     while (true) {
         string filename;
@@ -24,36 +33,33 @@ int main (void) {
         }
 
         try {
-            graph = WeightedGraph<long>::readFromFile(filename);
+            graph = WeightedGraph<long long>::readFromFile(filename);
             cout << "Graph loaded successfully from " << filename << endl;
         } catch (const runtime_error& e) {
             cout << "Error: " << e.what() << ". Try running again with correct graph." << endl;
             break; 
         }
-        // bool valid;
+
         while (true) {
             double x_1, y_1, x_2, y_2;
             string coord;
-            cout << "Enter a start coordinate (formatted as 'x y') or 'q' to quit: ";
-            //cin >> x_1 >> y_1;
 
+            cout << "Enter a start coordinate (formatted as 'x y') or 'q' to quit: ";
             getline(cin, coord);
-            cout << "input: " << coord << endl;
+
             if (coord == "q") {
                 cout << "Exiting program." << endl;
                 break;
             }
-            else {
+            else { 
                 stringstream StoD(coord);
                 StoD >> x_1 >> y_1;
-                // cout << "First number: " << x_1 << endl;
-                // cout << "Second number: " << y_1 << endl;
-                //cout << typeid(x_1).name() << endl;
+                // add test case for edge in adjList
             }
 
             cout << "Enter an end coordinate (formatted as 'x y') or 'q' to quit: ";
-            // cin >> x_2 >> y_2;
             getline(cin, coord);
+
             if (coord == "q") {
                 cout << "Exiting program." << endl;
                 break;
@@ -61,22 +67,23 @@ int main (void) {
             else {
                 stringstream StoD(coord);
                 StoD >> x_2 >> y_2;
-                // cout << "First number: " << x_1 << endl;
-                // cout << "Second number: " << y_1 << endl;
-                //cout << typeid(x_1).name() << endl;
+                // add test case for edge in adjList
             }
 
-            cout << "start coordinates are: " << x_1 << ", " << y_1 << endl;
-            cout << "end coordinates are: " << x_2 << ", " << y_2 << endl;
-        
+            //Start Dijkstra's and find shortest path
+            cout << "\n >>Finding shortest path from: " <<
+            "(" << x_1 << ", " << y_1 << ")" << " to (" <<
+            x_2 << ", " << y_2 << ")..." << endl;
 
+    
             pair<double, double> start = make_pair(x_1, y_1);
             pair<double, double> end = make_pair(x_2, y_2);
-            //call dijkstra's
+            //call dijkstra's with pairs
 
 
             
             break; 
         }
+        break; //prevent file prompt loop.
     }
 }
